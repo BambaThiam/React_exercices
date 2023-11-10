e82ecffceb29d010fd98035b41eade3bba4b8bcd
DayOneAI <bambathiam88@gmail.com>
2023-11-10 22:11:02 +0100

    HooksPersonnalisés

--- 01-React_reviews/src/06_Hooks/marvel.jsx
+++ 01-React_reviews/src/06_Hooks/marvel.jsx
@@ -1,8 +1,13 @@
-/* eslint-disable no-unused-vars */
 /* NE PAS MODIFIER CE FICHIER  */
+/* eslint-disable no-unused-vars */
 import * as React from 'react'
 
-const marvelApiKey = '6bcc5c7ff0ad488fb58f759c4069964c'
+// GET FREE KEY https://developer.marvel.com/account
+// add localhost to referrers
+// clef limité à 3000 appels par jour
+
+// const marvelApiKey = '6bcc5c7ff0ad488fb58f759c4069964c'
+const marvelApiKey = '5a4bd353e754599ed13518c2f87509df'
 
 const myHeader = new Headers({
   'Content-Type': 'application/x-www-form-urlencoded',
@@ -24,13 +29,46 @@ const fetchMarvel = (name) => {
         return Promise.reject(
           new Error(`Aucun Marvel trouvé avec le nom "${name}"`)
         )
-      }
+      } // ERROR DU JSON()
     })
     .catch((error) => {
       return Promise.reject(
         new Error(`Aucun Marvel trouvé avec le nom "${name}"`)
       )
-    }) // ERROR DU JSON()
+    }) // ERROR APPEL API
+}
+
+const fetchMarvelById = (id) => {
+  const url = `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${marvelApiKey}`
+  return fetch(url, init)
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.data.results.length > 0) {
+        return json.data.results[0]
+      } else {
+        return Promise.reject(
+          new Error(`Aucun Marvel trouvé avec l'id "${id}"`)
+        )
+      } // ERROR DU JSON()
+    })
+    .catch((error) => {
+      return Promise.reject(new Error(`Aucun Marvel trouvé avec l'id "${id}"`))
+    }) // ERROR APPEL API
+}
+
+const fetchMarvelsList = (name) => {
+  const url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${marvelApiKey}&nameStartsWith=${name}`
+  return fetch(url, init)
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.data.results.length > 0) {
+        return json.data.results
+      } else {
+        return Promise.reject(
+          new Error(`Aucun Marvel trouvé avec le nom "${name}"`)
+        )
+      } // ERROR DU JSON()
+    })
     .catch((error) => {
       return Promise.reject(
         new Error(`Aucun Marvel trouvé avec le nom "${name}"`)
@@ -81,18 +119,31 @@ function MarvelSearchForm({ onSearch, marvelName }) {
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="border-2 border-sky-300 m-2 p-1"
         />
         <input
           type="button"
           onClick={() => onSearch(name)}
           value={'rechercher'}
-          className="border-2 border-sky-300 m-2 p-1"
         />
       </div>
     </div>
   )
 }
 
-export { MarvelPersoView, MarvelSearchForm, fetchMarvel }
-// export { MarvelSearchForm, fetchMarvel }
+function ErrorDisplay({ error }) {
+  return (
+    <div style={{ color: 'red' }}>
+      Une erreur est survenue lors de la recherche de Marvel detail :{' '}
+      <pre style={{ color: 'grey' }}> Détail : {error.message}</pre>
+    </div>
+  )
+}
+
+export {
+  MarvelPersoView,
+  MarvelSearchForm,
+  fetchMarvel,
+  ErrorDisplay,
+  fetchMarvelById,
+  fetchMarvelsList,
+}
--- 01-React_reviews/src/07_Hooks_avances/HooksPersonnalises.jsx
+++ 01-React_reviews/src/07_Hooks_avances/HooksPersonnalises.jsx
@@ -1,44 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import {
   MarvelPersoView,
+  fetchMarvelById,
+  fetchMarvelsList,
   fetchMarvel,
   MarvelSearchForm,
+  ErrorDisplay,
 } from '../06_Hooks/marvel'
 import { ErrorBoundary } from 'react-error-boundary'
 
 const useMarvelExists = (marvelName) => {
   const [marvelExists, setMarvelExists] = useState(false)
-  const [error, setError] = useState(null)
+  //const [error, setError] = useState(null)
 
   useEffect(() => {
-    if (!marvelExists) {
+    if (!marvelName) {
       return
     }
-    setMarvelExists(false)
+    //setMarvelExists(false)
     fetchMarvel(marvelName)
       .then(() => setMarvelExists(true))
-      .catch((error) => setError(false))
-  }, [])
+      .catch(() => setMarvelExists(false))
+  }, [marvelName])
 
   return marvelExists
-
-  //   if (!marvelName) {
-  //     return <div>Entrer un nom de personnage Marvel</div>
-  //   }
-
-  //   if (error) {
-  //     throw error
-  //   }
-
-  //   if (!marvelExists) {
-  //     return <div>chargement ...</div>
-  //   }
-
-  //   return (
-  //     <div>
-  //       <MarvelPersoView marvel={marvelExists} />
-  //     </div>
-  //   )
 }
 
 const Marvel = ({ marvelName }) => {
@@ -48,17 +33,255 @@ const Marvel = ({ marvelName }) => {
   )
 }
 
-function ErrorDisplay({ error }) {
+const useFindMarvelByName = (marvelName) => {
+  const [marvel, setMarvel] = useState()
+  const [error, setError] = useState(null)
+  //Autre méthode avec reducer
+  const [state, dispatch] = useReducer(reducer, {
+    marvel: null,
+    error: null,
+  })
+
+  useEffect(() => {
+    if (!marvelName) {
+      return
+    }
+
+    fetchMarvel(marvelName)
+      .then((marvel) => setMarvel(marvel))
+      .catch((error) => setError(error))
+  }, [marvelName])
+
+  return [marvel, error]
+}
+
+const FindMarvel = ({ marvelName }) => {
+  const [marvel, error] = useFindMarvelByName(marvelName)
+  if (error) {
+    throw error
+  }
   return (
-    <div style={{ color: 'red' }}>
-      Une erreur est survenue lors de la recherche de Marvel detail :{' '}
-      <pre style={{ color: 'grey' }}> Détail : {error.message}</pre>
+    <div>
+      {' '}
+      {marvel
+        ? `Le marvel existe, son nom est : ${marvel.name}`
+        : `Le marvel n'existe pas`}
     </div>
   )
 }
 
+const reducer = (state, action) => ({ ...state, ...action })
+const useFindMarvelByNameReduce = (marvelName) => {
+  //Autre méthode avec reducer
+  const [state, dispatch] = useReducer(reducer, {
+    marvel: null,
+    error: null,
+  })
+
+  useEffect(() => {
+    if (!marvelName) {
+      return
+    }
+
+    fetchMarvel(marvelName)
+      .then((marvel) => dispatch({ marvel }))
+      .catch((error) => dispatch({ error }))
+  }, [marvelName])
+
+  return state
+}
+
+const FindMarvelReduce = ({ marvelName }) => {
+  const state = useFindMarvelByNameReduce(marvelName)
+  const { marvel, error } = state
+  if (error) {
+    throw error
+  }
+  return (
+    <div>
+      {' '}
+      {marvel ? <MarvelPersoView marvel={marvel} /> : `Le marvel n'existe pas`}
+    </div>
+  )
+}
+
+// Avec Reducer & Payload
+const reducer1 = (state, action) => {
+  switch (action.type) {
+    case 'FETCHING':
+      return { marvel: null, error: null }
+    case 'DONE':
+      return { marvel: action.payload, error: null }
+    case 'FAIL':
+      return { marvel: null, error: action.error }
+    default:
+      throw new Error('Action non supportée')
+  }
+}
+const useFindMarvelByNameReducePayload = (marvelName) => {
+  //Autre méthode avec reducer & Payload
+  const [state, dispatch] = useReducer(reducer1, {
+    marvel: null,
+    error: null,
+  })
+
+  useEffect(() => {
+    if (!marvelName) {
+      return
+    }
+    dispatch({ type: 'FETCHING' })
+    fetchMarvel(marvelName)
+      .then((marvel) => dispatch({ type: 'DONE', payload: marvel }))
+      .catch((error) => dispatch({ type: 'FAIL', error }))
+  }, [marvelName])
+
+  return state
+}
+
+const FindMarvelReducePayload = ({ marvelName }) => {
+  const state = useFindMarvelByNameReducePayload(marvelName)
+  const { marvel, error } = state
+  if (error) {
+    throw error
+  }
+  return (
+    <div>
+      {' '}
+      {marvel ? <MarvelPersoView marvel={marvel} /> : `Le marvel n'existe pas`}
+    </div>
+  )
+}
+
+// Gérer le chargement avec un status
+const reducer2 = (state, action) => {
+  switch (action.type) {
+    case 'FETCHING':
+      return { status: 'FETCHING', marvel: null, error: null }
+    case 'DONE':
+      return { status: 'DONE', marvel: action.payload, error: null }
+    case 'FAIL':
+      return { status: 'FAIL', marvel: null, error: action.error }
+    default:
+      throw new Error('Action non supportée')
+  }
+}
+const useFindMarvelByNameReducePayloadStatut = (marvelName) => {
+  //Autre méthode avec reducer & Payload
+  const [state, dispatch] = useReducer(reducer2, {
+    marvel: null,
+    error: null,
+    status: 'IDLE',
+  })
+
+  useEffect(() => {
+    if (!marvelName) {
+      return
+    }
+    dispatch({ type: 'FETCHING' })
+    fetchMarvel(marvelName)
+      .then((marvel) => dispatch({ type: 'DONE', payload: marvel }))
+      .catch((error) => dispatch({ type: 'FAIL', error }))
+  }, [marvelName])
+
+  return state
+}
+
+const FindMarvelReducePayloadStatut = ({ marvelName }) => {
+  const state = useFindMarvelByNameReducePayloadStatut(marvelName)
+  const { marvel, error, status } = state
+  if (error) {
+    throw error
+  } else if (status === 'IDLE') {
+    return 'Enter un nom de Marvel'
+  } else if (status === 'FETCHING') {
+    return 'chargement en cours ...'
+  } else if (status === 'DONE') {
+    return <MarvelPersoView marvel={marvel} />
+  }
+}
+
+// Fetch générique
+const reducer3 = (state, action) => {
+  switch (action.type) {
+    case 'FETCHING':
+      return { status: 'FETCHING', data: null, error: null }
+    case 'DONE':
+      return { status: 'DONE', data: action.payload, error: null }
+    case 'FAIL':
+      return { status: 'FAIL', data: null, error: action.error }
+    default:
+      throw new Error('Action non supportée')
+  }
+}
+const useFindMarvelNameFetch = (search, fetch) => {
+  //Autre méthode avec reducer & Payload
+  const [state, dispatch] = useReducer(reducer3, {
+    marvel: null,
+    error: null,
+    status: 'IDLE',
+  })
+
+  useEffect(() => {
+    if (!search) {
+      return
+    }
+    dispatch({ type: 'FETCHING' })
+    fetch(search)
+      .then((result) => dispatch({ type: 'DONE', payload: result }))
+      .catch((error) => dispatch({ type: 'FAIL', error }))
+  }, [fetch, search])
+
+  return state
+}
+
+function useFindMarvelByNameBis(marvelName) {
+  return useFindMarvelNameFetch(marvelName, fetchMarvel)
+}
+
+function useFindMarvelList(marvelName) {
+  return useFindMarvelNameFetch(marvelName, fetchMarvelsList)
+}
+
+const FindMarvelFetchGenerique = ({ marvelName }) => {
+  const state = useFindMarvelByNameBis(marvelName)
+  const { data: marvel, error, status } = state
+  if (error) {
+    throw error
+  } else if (status === 'IDLE') {
+    return 'Enter un nom de Marvel'
+  } else if (status === 'FETCHING') {
+    return 'chargement en cours ...'
+  } else if (status === 'DONE') {
+    return <MarvelPersoView marvel={marvel} />
+  }
+}
+
+const MarvelList = ({ marvelName }) => {
+  const state = useFindMarvelList(marvelName)
+  const { data: marvels, error, status } = state
+  if (error) {
+    throw error
+  } else if (status === 'IDLE') {
+    return 'Enter un nom de Marvel'
+  } else if (status === 'FETCHING') {
+    return 'chargement en cours ...'
+  } else if (status === 'DONE') {
+    return (
+      <>
+        {marvels.map((marvel) => (
+          <div key={marvel.id}>
+            <hr style={{ background: 'grey' }} />
+            <MarvelPersoView marvel={marvel} />
+          </div>
+        ))}
+      </>
+    )
+  }
+}
+
 const HooksPersonnalises = () => {
   const [marvelName, setMarvelName] = useState('')
+  const [searchList, setSearchList] = useState(false)
   const handleSearch = (name) => {
     setMarvelName(name)
   }
@@ -70,12 +293,70 @@ const HooksPersonnalises = () => {
         Hook simple qui nous permettrais de savoir si un personnage Marvel exist
         que l'on pourrait utiliser comme cela.
       </h6>
-      <h6>-------------------------------------</h6>
+      <h6>
+        ------------------Hooks Personnalisés : Logique
+        réutilisable-------------------
+      </h6>
       <div>
         <MarvelSearchForm marvelName={marvelName} onSearch={handleSearch} />
         <div>
           <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
             <Marvel marvelName={marvelName} />
+          </ErrorBoundary>
+        </div>
+        <h6>
+          -------------------Hook avec Personnage / erreur------------------
+        </h6>
+        <div>
+          <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
+            <FindMarvel marvelName={marvelName} />
+          </ErrorBoundary>
+        </div>
+        <h6>-------------------useReducer------------------</h6>
+        <div>
+          <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
+            <FindMarvelReduce marvelName={marvelName} />
+          </ErrorBoundary>
+        </div>
+        <h6>------------------Type d'action et payload-------------------</h6>
+        <div>
+          <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
+            <FindMarvelReducePayload marvelName={marvelName} />
+          </ErrorBoundary>
+        </div>
+        <h6>
+          ------------------Gérer le chargement avec un
+          status-------------------
+        </h6>
+        <div>
+          <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
+            <FindMarvelReducePayloadStatut marvelName={marvelName} />
+          </ErrorBoundary>
+        </div>
+        <h6>------------------Fetch générique-------------------</h6>
+        <h6>
+          Dans cet exercice il va falloir créer un hook `useFindMarvelList` qui
+          utilise la fonction `fetchMarvelsList` (à importer depuis `marvel.js`)
+          et la passe en paramètre de `useFetchData`. Ce hook sera appelé depuis
+          un nouveau composant `MarvelList`. Ajouter une `checkBox` avec un
+          state `searchList` et conditionner l'affichage.
+        </h6>
+        <label>
+          <input
+            type="checkbox"
+            checked={searchList}
+            onChange={(e) => setSearchList(e.target.checked)}
+          />
+          Chercher dans une liste ?
+        </label>
+        <MarvelSearchForm marvelName={marvelName} onSearch={handleSearch} />
+        <div>
+          <ErrorBoundary key={marvelName} FallbackComponent={ErrorDisplay}>
+            {searchList ? (
+              <MarvelList marvelName={marvelName} />
+            ) : (
+              <FindMarvelFetchGenerique marvelName={marvelName} />
+            )}
           </ErrorBoundary>
         </div>
       </div>
