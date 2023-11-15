035974e35959968859abfb143c27829d4cb08982
DayOneAI <bambathiam88@gmail.com>
2023-11-15 13:27:55 +0100

    Step_Setup

--- 01-React_reviews/src/99-Rereview/Step.jsx
+++ 01-React_reviews/src/99-Rereview/Step.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import './data/step.css'
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+]
 
 const Step = () => {
-  return <div>Step</div>
+  const [step, setStep] = React.useState(1)
+
+  const handlePrevious = () => {
+    if (step > 1) {
+      return setStep(step - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if (step < 3) {
+      return setStep(step + 1)
+    }
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`number ${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`number ${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`number ${step >= 3 && step < 4 ? 'active' : ''}`}>
+          3
+        </div>
+      </div>
+      <p className="message">
+        {' '}
+        Step: {step} {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: '#7950f2', color: 'white' }}
+          onClick={() => handlePrevious()}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: '#7950f2', color: 'white' }}
+          onClick={() => handleNext()}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  )
 }
+
 
 export default Step
