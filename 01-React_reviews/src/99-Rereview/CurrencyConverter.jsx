77dcfba8ab62d6e37e27cb5445f237205785cae6
DayOneAI <bambathiam88@gmail.com>
2023-12-13 00:08:54 +0100

    Currency_Converter_end

--- 01-React_reviews/src/99-Rereview/CurrencyConverter.jsx
+++ 01-React_reviews/src/99-Rereview/CurrencyConverter.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 const CurrencyConverter = () => {
-  const [amount, setAmount] = React.useState(0)
-  const [from, setFrom] = React.useState('USD')
-  const [to, setTo] = React.useState('EUR')
-  const [result, setResult] = React.useState(0)
+  const [amount, setAmount] = useState(1)
+  const [from, setFrom] = useState('EUR')
+  const [to, setTo] = useState('USD')
+  const [result, setResult] = useState(3)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleChangeAmount = (event) => {
     setAmount(Number(event.target.value))
@@ -22,25 +23,48 @@ const CurrencyConverter = () => {
 
   const URL = `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
 
+  useEffect(() => {
+    setIsLoading(true)
+    const getCurrencyRate = async () => {
+      const response = await fetch(URL)
+      const data = await response.json()
+
+      if (from === to) {
+        setResult(amount)
+      } else {
+        setResult(`${amount} ${from} = ${data.rates[to]} ${to}`)
+      }
+    }
+    setIsLoading(false)
+
+    getCurrencyRate()
+  }, [amount, from, to, URL])
+
   return (
     <>
       <div>Currency Converter</div>
       <div className="flex flex-row gap-2 justify-center">
-        <input type="text" onChange={handleChangeAmount} />
-        <select onChange={handleChangeFrom} value={from}>
+        <input
+          type="text"
+          value={amount}
+          onChange={handleChangeAmount}
+          disabled={isLoading}
+        />
+
+        <select onChange={handleChangeFrom} value={from} disabled={isLoading}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
           <option value="INR">INR</option>
         </select>
 
-        <select onChange={handleChangeTo} value={to}>
+        <select onChange={handleChangeTo} value={to} disabled={isLoading}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
           <option value="INR">INR</option>
         </select>
-        <p className="ml-10">OUTPUT</p>
+        <p className="ml-10">{result}</p>
       </div>
     </>
   )
