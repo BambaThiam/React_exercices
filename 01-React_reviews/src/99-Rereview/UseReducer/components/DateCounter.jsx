f5c64d24fff5478f05e44abb2526b1bfafc7ff6c
DayOneAI <bambathiam88@gmail.com>
2023-12-25 14:57:50 +0100

    UsereducerDateCounter

--- 01-React_reviews/src/99-Rereview/UseReducer/components/DateCounter.jsx
+++ 01-React_reviews/src/99-Rereview/UseReducer/components/DateCounter.jsx
@@ -1,49 +1,48 @@
 import { useReducer, useState } from 'react'
 
+const initialState = { count: 0, step: 1 }
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'inc':
-      return state + 1
     case 'dec':
-      return state - 1
+      return { ...state, count: state.count - state.step }
+    case 'inc':
+      return { ...state, count: state.count + state.step }
     case 'setCount':
-      return action.payload
+      return { ...state, count: action.payload }
+    case 'setStep':
+      return { ...state, step: action.payload }
+    case 'reset':
+      return initialState
     default:
-      return state
+      throw new Error('Action inconnue')
   }
 }
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0)
-
-  const [step, setStep] = useState(1)
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { count, step } = state
 
   // This mutates the date object.
   const date = new Date('june 21 2027')
   date.setDate(date.getDate() + count)
 
   const dec = function () {
-    // setCount((count) => count - 1);
     dispatch({ type: 'dec' })
   }
 
   const inc = function () {
-    // setCount((count) => count + 1);
     dispatch({ type: 'inc' })
   }
 
   const defineCount = function (e) {
-    // setCount(Number(e.target.value))
     dispatch({ type: 'setCount', payload: Number(e.target.value) })
   }
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value))
+    dispatch({ type: 'setStep', payload: Number(e.target.value) })
   }
 
   const reset = function () {
-    setCount(0)
-    setStep(1)
+    dispatch({ type: 'reset' })
   }
 
   return (
@@ -73,4 +72,5 @@ function DateCounter() {
     </div>
   )
 }
+
 export default DateCounter
